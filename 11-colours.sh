a75c8fe57d5ee5a267cee184f68abf1f5b16f5d1
saivenkatasiva <msaivenkatasiva@gmail.com>
2024-04-25 16:17:21 +0530

    something

--- 11-colours.sh
+++ 11-colours.sh
@@ -5,13 +5,17 @@ USERID=$(id -u) #here we are checking it is super user or not
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+echo "script strted executing at : $TIMESTAMP"
 validate() 
 {   #here we are validating the exit status of every installing package
     if [ $1 -ne 0]  #we are checking if first argument that is exit status of previous command is zero or not
     then 
-    echo "$2...failure"  #if not second argument is showing as failure
+    echo -e "$2...$R failure $N"  #if not second argument is showing as failure
     exit 1
-    echo "$2 success"  #if yes means its success
+    echo -e "$2 $G success $N"  #if yes means its success
 }
 if [ $USERID -ne 0 ] #id -u must be 0 for root access
 then
